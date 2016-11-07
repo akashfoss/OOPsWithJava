59bba1690b60411886497455e614d4c93026a389
akashfoss <akashkrishnan705@gmail.com>
2016-11-07 21:19:20 +0530

    Added setter and getter

---
+++ fraction.java
@@ -0,0 +1,21 @@
+public class fraction{
+private int num;
+private int den;
+
+
+public void setnum(int n){
+this.num = n;
+}
+
+public void setden(int d){
+this.den = d;
+}
+
+public int getnum(){
+return num;
+}
+
+public int getden(){
+return den;
+}
+	}
