6f266ad10525a832468bbf3a5c74db37981f0e3a
akashfoss <akashkrishnan705@gmail.com>
2016-11-07 21:31:44 +0530

    added main function to the same program

---
+++ fraction2.java
@@ -0,0 +1,29 @@
+public class fraction2{
+private int num;
+private int den;
+
+
+public void setnum(int n){
+this.num = n;
+}
+
+public void setden(int d){
+this.den = d;
+}
+
+public int getnum(){
+return num;
+}
+
+public int getden(){
+return den;
+}
+
+public static void main (String [] args){
+fraction2 f = new fraction2();
+f.setnum(4);
+f.setden(5);
+System.out.println("numerator/denominator"+f.getnum()+"/"+f.getden());
+
+}
+	}
