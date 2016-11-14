9eb7f9295702b6e9844b72977a31450c6cc325ff
akashfoss <akashkrishnan705@gmail.com>
2016-11-14 23:23:04 +0530

    fraction upgrades

--- fraction.java
+++ fraction.java
@@ -18,4 +18,13 @@ return num;
 public int getden(){
 return den;
 }
+
+public static void main (String [] args){
+fraction f = new fraction();
+f.setnum(4);
+f.setden(5);
+System.out.println("numerator/denominator"+f.getnum()+"/"+f.getden());
+
+}
+
 	}
